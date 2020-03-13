f7b7240221b18fee1b12f4e329b637c95c43310f
Gordiig <gordiig@gmail.com>
2020-03-13 20:21:37 +0300

    Add model class

--- Pins/models.py
+++ Pins/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+
+class Pin(models.Model):
+    """
+    Модель пина (штуки на карте)
+    """
+    PLACE_PIN, USER_PIN = 'p', 'u'
+    PIN_TYPE_CHOICES = (
+        (PLACE_PIN, 'Место'),
+        (USER_PIN, 'Юзер'),
+    )
+
+    name = models.CharField(max_length=128, null=False, blank=False)
+    descr = models.CharField(max_length=1024, null=True, blank=True)
+    ptype = models.CharField(choices=PIN_TYPE_CHOICES, max_length=2, null=False)
+    price = models.IntegerField(null=False, blank=False)
+    img_link = models.URLField(null=True, blank=True)
+    created_dt = models.DateTimeField(auto_now_add=True)
+    deleted_flg = models.BooleanField(default=False, null=False)
+
+    def __str__(self):
+        return f'{self.ptype}pin {self.name}'
