ffc12a8b70c7c02f15790a36527ed77625108632
Gordiig <gordiig@gmail.com>
2020-03-13 20:48:16 +0300

    Add migration

--- AwardsService/settings.py
+++ AwardsService/settings.py
@@ -128,7 +128,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/static/')
+
 STATIC_URL = '/static/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+MEDIA_URL = '/media/'
 
 
 try:
