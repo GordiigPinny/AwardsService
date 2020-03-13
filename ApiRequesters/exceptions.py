9e6adffbdf4e9e3cec78e0cca44d9bf98411844b
Gordiig <gordiig@gmail.com>
2020-03-13 22:56:07 +0300

    Add superuser check to views

--- ApiRequesters/exceptions.py
+++ ApiRequesters/exceptions.py
@@ -5,16 +5,18 @@ class BaseApiRequestError(Exception):
     """
     Базовый класс для остальных эксепшнов
     """
+    def __init__(self, message: str = 'BaseApiRequestError was raised'):
+        self.message = message
 
 
 class RequestError(BaseApiRequestError):
     def __init__(self, message: str = 'Request error'):
-        self.message = message
+        super().__init__(message=message)
 
 
 class UnexpectedResponse(BaseApiRequestError):
     def __init__(self, response: requests.Response, message: str = 'Неожиданный ответ с сервера'):
-        self.message = message
+        super().__init__(message=message)
         self.response = response
         self.code = response.status_code
         try:
@@ -25,5 +27,5 @@ class UnexpectedResponse(BaseApiRequestError):
 
 class JsonDecodeError(BaseApiRequestError):
     def __init__(self, body_text: str, message: str = 'Couldn\'t decode response\'s body as json'):
+        super().__init__(message=message)
         self.body_text = body_text
-        self.message = message
--- Pins/views.py
+++ Pins/views.py
@@ -4,6 +4,10 @@ from rest_framework.generics import ListCreateAPIView
 from rest_framework.pagination import LimitOffsetPagination
 from Pins.models import Pin
 from Pins.serializers import PinsListSerializer, PinDetailSerializer
+from Pins.permissions import IsAuthenticated
+from Pins.utils import get_token_from_request
+from ApiRequesters.Auth.AuthRequester import AuthRequester
+from ApiRequesters.exceptions import BaseApiRequestError
 
 
 class PinListView(ListCreateAPIView):
@@ -12,18 +16,29 @@ class PinListView(ListCreateAPIView):
     """
     serializer_class = PinsListSerializer
     pagination_class = LimitOffsetPagination
-    permission_classes = ()
+    permission_classes = (IsAuthenticated, )
+
+    def is_superuser(self, request: Request):
+        try:
+            return AuthRequester().is_superuser(get_token_from_request(request))[1]
+        except BaseApiRequestError as e:
+            # TODO: - Вот тут логи собирать
+            return False
 
     def get_queryset(self):
         ptype = self.request.query_params.get('ptype', None)
-        deleted_flg = False  # TODO: - Если админ, то вытягивать из query_params
+        with_deleted = False if not self.is_superuser(self.request) \
+            else self.request.query_params.get('show_deleted', False)
         if ptype is None:
-            return Pin.objects.filter(deleted_flg=deleted_flg)
+            return Pin.objects.all() if with_deleted \
+                else Pin.objects.filter(deleted_flg=False)
         else:
-            return Pin.objects.filter(deleted_flg=deleted_flg, ptype=ptype)
+            return Pin.objects.filter(ptype=ptype) if with_deleted \
+                else Pin.objects.filter(ptype=ptype, deleted_flg=False)
 
     def create(self, request: Request, *args, **kwargs):
-        # TODO: - Сюда на определение админа проверка
+        if not self.is_superuser(request):
+            return Response({'error', 'Only admins can post pins'}, status=status.HTTP_403_FORBIDDEN)
         return super().create(request, args, kwargs)
 
 
@@ -31,19 +46,29 @@ class PinDetailView(APIView):
     """
     Вьюха для возврата пина
     """
-    permission_classes = ()
+    permission_classes = (IsAuthenticated, )
+
+    def is_superuser(self, request: Request):
+        try:
+            return AuthRequester().is_superuser(get_token_from_request(request))[1]
+        except BaseApiRequestError as e:
+            # TODO: - Вот тут логи собирать
+            return False
 
     def get(self, request: Request, pk):
-        deleted_flg = False  # TODO: - Если админ, то вытягивать из query_params
+        with_deleted = False if not self.is_superuser(request) \
+            else request.query_params.get('show_deleted', False)
         try:
-            pin = Pin.objects.get(pk=pk, deleted_flg=deleted_flg)
+            pin = Pin.objects.get(pk=pk) if with_deleted \
+                else Pin.objects.get(pk=pk, deleted_flg=False)
         except Pin.DoesNotExist:
             return Response(status=status.HTTP_404_NOT_FOUND)
         serializer = PinDetailSerializer(instance=pin)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
     def patch(self, request: Request, pk):
-        # TODO: - Сюда на определение админа проверка
+        if not self.is_superuser(request):
+            return Response({'error', 'Only admin can modify pins'}, status=status.HTTP_403_FORBIDDEN)
         try:
             pin = Pin.objects.get(pk=pk, deleted_flg=False)
         except Pin.DoesNotExist:
@@ -55,7 +80,8 @@ class PinDetailView(APIView):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     def delete(self, request: Request, pk):
-        # TODO: - Сюда на определение админа проверка
+        if not self.is_superuser(request):
+            return Response({'error', 'Only admin can modify pins'}, status=status.HTTP_403_FORBIDDEN)
         try:
             pin = Pin.objects.get(pk=pk, deleted_flg=False)
         except Pin.DoesNotExist:
